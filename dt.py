e1b911eccb5405f521bf44139297ebdb0e52a4f9
Prithvi Arunshankar <prithvia05@Prithvis-MBP-2.attlocal.net>
2025-06-26 10:16:23 -0700

    Zeroed out noise land values and updated config file

--- dt.py
+++ dt.py
@@ -27,7 +27,7 @@ dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
 model = DiffusionModel().to(device)
 diffusion = Diffusion(model, num_steps=1000, beta_0=1e-4, beta_f=0.02, device=device)
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
-num_epochs = 10
+num_epochs = 20
 epoch_losses = []
 cumulative_mses = []
 cumulative_mse_loss = 0.0
@@ -49,6 +49,7 @@ for epoch in range(num_epochs):
     # Start Training and Keep Track of Loss
     model.train()
     curr_epoch_loss = 0.0
+    every_10_batch_loss = 0.0
 
     # Go through each of the 548 batches of size 16
     for batch_idx, data in enumerate(dataloader):
@@ -56,6 +57,7 @@ for epoch in range(num_epochs):
         # Get the 16 samples from the dataloader
         batch_start_time = time.perf_counter()
         batch_loss = 0.0
+        
         x_0 = data.to(device).float()
         batch_size = x_0.size(0)
 
@@ -77,6 +79,7 @@ for epoch in range(num_epochs):
 
         # Set the noise land values to 0
         noise_pred[mask_expanded] = 0
+        noise[mask_expanded] = 0
 
         #with record_function("calculate_MSE_loss"):
         # Calculate the loss
@@ -85,6 +88,7 @@ for epoch in range(num_epochs):
         loss = mse_loss.sum() / ocean_elements
         curr_epoch_loss += loss.item()
         batch_loss += loss.item()
+        every_10_batch_loss += loss.item()
 
         #with record_function("backward_propagation"):
         # Apply backward propagation
@@ -93,9 +97,10 @@ for epoch in range(num_epochs):
 
         # If at the end of the batch, print step and calculated loss
         if batch_idx % 54 == 53:
-            average_loss = curr_epoch_loss / 54
+            average_loss = every_10_batch_loss / 54
             print(f"Epoch [{epoch+1}/{num_epochs}], Step [{batch_idx+1}], Loss: {average_loss:.10f}")
             total_loss = 0.0
+            every_10_batch_loss = 0.0
         
         batch_time = time.perf_counter() - batch_start_time
         print(f"Batch: {batch_idx}, Batch Time: {batch_time}, Batch Loss: {batch_loss}")
